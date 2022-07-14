2da143b52c5c07ebfd819fc78bb348017fea731d
Joanis Rouanet <joanis.ang3@gmail.com>
2022-07-14 19:51:10 +0200

    Fixed scripts

--- install.sh
+++ install.sh
@@ -7,3 +7,12 @@ wget https://files.zimbra.com/downloads/8.8.15_GA/zcs-8.8.15_GA_4179.UBUNTU20_64
 tar xvfz zcs-8.8.15_GA_4179.UBUNTU20_64.20211118033954
 cd zcs-8.8.15_GA_4179.UBUNTU20_64.20211118033954
 sudo ./install.sh
+
+echo -e "\033[33;1mSetting DNS\033[0m"
+sudo apt install -y resolvconf
+sudo echo "nameserver 8.8.8.8" | sudo tee -a /etc/resolvconf/resolv.conf.d/tail
+sudo echo "nameserver 8.8.4.4" | sudo tee -a /etc/resolvconf/resolv.conf.d/tail
+
+echo -e "\033[33;1mSystem reboot\033[0m"
+echo "System reboot."
+sudo reboot
--- prepare.sh
+++ prepare.sh
@@ -5,8 +5,8 @@ echo -e "\033[41;37;1m /!\ ZIMBRA SERVER PRE-INSTALLATION /!\\" "\\033[0m"
 
 if [ -z "$1" ]
 then
-	echo -e "Missing argument #1: domain"
-	echo
+	echo -e "Missing argument #1: domain."
+	exit
 else
 	ZIMBRA_SERVER_DOMAIN=$1
 	echo -e "Domain:" $1
@@ -14,18 +14,14 @@ else
 fi
 
 echo -e "\033[33;1mSystem updates\033[0m"
-sudo apt update && sudo apt upgrade
-sudo apt install net-tools build-essential sqlite3 sysstat ntp libaio1 pax
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y net-tools build-essential sqlite3 sysstat ntp libaio1 pax
 
 echo -e "\033[33;1mSetting hostname\033[0m"
 sudo hostnamectl set-hostname $ZIMBRA_SERVER_DOMAIN
 sudo sed -i "s/\(preserve_hostname: *\).*/\1true/" /etc/cloud/cloud.cfg
 sudo echo "127.0.0.1 $(hostname --fqdn)" | sudo tee -a /etc/hosts
 
-echo -e "\033[33;1mSetting DNS\033[0m"
-sudo echo "nameserver 8.8.8.8" | sudo tee -a /etc/resolvconf/resolv.conf.d/tail
-sudo echo "nameserver 8.8.4.4" | sudo tee -a /etc/resolvconf/resolv.conf.d/tail
-
-echo -e "\033[33;1mSystem reboot DNS\033[0m"
-echo "The system reboot. When restarted, run the script 'install.sh'."
+echo -e "\033[33;1mSystem reboot\033[0m"
+echo "System reboot. When restarted, run the script 'install.sh'."
 sudo reboot
